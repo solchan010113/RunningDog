2ef86f334c2d79d2ea704dbca709a0bcd785c07a
ericshin215 <nuevo0703@gmail.com>
2023-11-02 10:07:08 +0900

    1102

--- src/main/java/com/runningdog/controller/WalkBlogController.java
+++ src/main/java/com/runningdog/controller/WalkBlogController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WalkBlogController {
 	
 	
+	
+	
+	@RequestMapping(value = "home")
 	public String home() {
 		return"walkBlog/walkBlogHome";
+	}
+	
+	@RequestMapping(value = "userBlog")
+	public String userBlog() {
+		return"walkBlog/userBlog";
 	}
 
 }
