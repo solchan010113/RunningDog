4d173979edddf35af49b31bd08996dd607734386
ericshin215 <nuevo0703@gmail.com>
2023-11-03 18:36:57 +0900

    1103

--- src/main/java/com/runningdog/vo/UserLikeVo.java
+++ src/main/java/com/runningdog/vo/UserLikeVo.java
@@ -2,70 +2,60 @@ package com.runningdog.vo;
 
 public class UserLikeVo {
 	
-	private int likeNo;
+	private int userlikeNo;
 	private UsersVo userNo;
 	private String type;
 	private String useNo;
 	
 	public UserLikeVo() {
-
+		
 	}
 	
-	
-	public UserLikeVo(int likeNo, UsersVo userNo, String type, String useNo) {
+	public UserLikeVo(int userlikeNo, UsersVo userNo, String type, String useNo) {
 		super();
-		this.likeNo = likeNo;
+		this.userlikeNo = userlikeNo;
 		this.userNo = userNo;
 		this.type = type;
 		this.useNo = useNo;
 	}
 
-
-	public int getLikeNo() {
-		return likeNo;
+	public int getUserlikeNo() {
+		return userlikeNo;
 	}
 
-
-	public void setLikeNo(int likeNo) {
-		this.likeNo = likeNo;
+	public void setUserlikeNo(int userlikeNo) {
+		this.userlikeNo = userlikeNo;
 	}
-
 
 	public UsersVo getUserNo() {
 		return userNo;
 	}
 
-
 	public void setUserNo(UsersVo userNo) {
 		this.userNo = userNo;
 	}
-
 
 	public String getType() {
 		return type;
 	}
 
-
 	public void setType(String type) {
 		this.type = type;
 	}
-
 
 	public String getUseNo() {
 		return useNo;
 	}
 
-
 	public void setUseNo(String useNo) {
 		this.useNo = useNo;
 	}
 
-
 	@Override
 	public String toString() {
-		return "UserLike [likeNo=" + likeNo + ", type=" + type + ", useNo=" + useNo + "]";
+		return "UserLikeVo [userlikeNo=" + userlikeNo + ", userNo=" + userNo + ", type=" + type + ", useNo=" + useNo
+				+ "]";
 	}
-	
 	
 	
 	
